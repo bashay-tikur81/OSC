fc9947dfba05039f64cd9927ddd1b9575d2e02c1
Ertale81 <tikurbashay81@gmail.com>
2024-11-01 16:23:35 +0300

    committing org mode TODO list

---
+++ Learning.java
@@ -0,0 +1,9 @@
+class Learning{
+    public static void main(String[] args){
+        System.out.println("This is inside emacs org mode");
+        for(int i = 0; i < 5; i++){
+            System.out.println(i);
+
+    }
+}
+}
